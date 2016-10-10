f8beae7e1c44e14898e292b902c0a95b947323d4
Joe Kay <joe.kay.iv@gmail.com>
2016-10-10 09:02:22 -0400

    Added basic interaction, plus some margin tweaks

--- app/src/main/java/autotext/app/AutoReply.java
+++ app/src/main/java/autotext/app/AutoReply.java
@@ -7,6 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 /**
@@ -17,9 +22,15 @@ import android.view.ViewGroup;
  * Use the {@link AutoReply#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AutoReply extends Fragment {
+public class AutoReply extends Fragment implements OnClickListener {
 
     private OnFragmentInteractionListener mListener;
+
+    private EditText editText;
+    private EditText editDuration;
+    private RadioButton minutesRadioButton;
+    private RadioButton hoursRadioButton;
+    private Button saveButton;
 
     public AutoReply() {
         // Required empty public constructor
@@ -38,7 +49,18 @@ public class AutoReply extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auto_reply, container, false);
+        View view = inflater.inflate(R.layout.fragment_auto_reply, container, false);
+
+        editText = (EditText) view.findViewById(R.id.auto_reply_text);
+        editDuration = (EditText) view.findViewById(R.id.auto_reply_duration);
+        minutesRadioButton = (RadioButton) view.findViewById(R.id.auto_reply_minutes);
+        hoursRadioButton = (RadioButton) view.findViewById(R.id.auto_reply_hours);
+        saveButton = (Button) view.findViewById(R.id.auto_reply_save_button);
+
+        minutesRadioButton.setChecked(true);
+        saveButton.setOnClickListener(this);
+
+        return view;
     }
 
     @Override
@@ -56,6 +78,26 @@ public class AutoReply extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String text = editText.getText().toString();
+        String durationText = editDuration.getText().toString();
+        boolean useMinutes = minutesRadioButton.isChecked();
+        boolean useHours = hoursRadioButton.isChecked();
+
+        if(text.isEmpty()) {
+            editText.setError("Message is required");
+            return;
+        }
+
+        if(durationText.isEmpty()) {
+            editDuration.setError("Duration is required");
+        }
+
+        //TODO: Navigation to another view
     }
 
     /**
