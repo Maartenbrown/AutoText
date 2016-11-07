69b3a9b7b831e917b9a26915f024797493d3e3c0
Kardif <maartenbrown@gmail.com>
2016-11-07 12:52:07 -0500

    User login finished

--- app/src/main/java/autotext/app/AutoReplyDBHelper.java
+++ app/src/main/java/autotext/app/AutoReplyDBHelper.java
@@ -22,7 +22,7 @@ public class AutoReplyDBHelper extends SQLiteOpenHelper {
     public static final String T2C1 = "name";
     public static final String T2C2 ="uID_User";
     private static final String CreateT2 = "CREATE TABLE "+T2Name+" ("+T2Key+" INTEGER PRIMARY KEY, "
-            +T2C1+ " VARCHAR(50), FOREIGN KEY ("+T2C2+") REFERENCES "+T1Name+"("+T1Key+"));";
+            +T2C1+ " VARCHAR(50), "+T2C2+" INTEGER, FOREIGN KEY ("+T2C2+") REFERENCES "+T1Name+"("+T1Key+"));";
     public static final String T3Name="ReceivedMessages";//Received Messages Table
     public static final String T3Key ="rMessageID";
     public static final String T3C1 ="time";
@@ -30,9 +30,9 @@ public class AutoReplyDBHelper extends SQLiteOpenHelper {
     public static final String T3C3 ="uID_Users";
     public static final String T3C4 = "cID_Contact";
     private static final String CreateT3 ="CREATE TABLE "+T3Name+" ("+T3Key+" INTEGER PRIMARY KEY, "+
-            T3C1+" TEXT DEFAULT CURRENT_TIMESTAMP, "+
-            T3C2+" VARCHAR, FOREIGN KEY ("+T3C3+") REFERENCES "+T1Name+"("+T1Key+"), "+
-            "FOREIGN KEY ("+T3C4+") REFERENCES "+T2Name+"("+T2Key+"));";
+            T3C1+" TEXT, "+
+            T3C2+" VARCHAR, "+T3C3+" INTEGER, "+
+            T3C4+" INTEGER, FOREIGN KEY ("+T3C3+") REFERENCES "+T1Name+"("+T1Key+"), FOREIGN KEY ("+T3C4+") REFERENCES "+T2Name+"("+T2Key+"));";
     public static final String T4Name="SentMessages";//Sent Messages Table
     public static final String T4Key ="sMessageID";
     public static final String T4C1 ="time";
@@ -41,13 +41,13 @@ public class AutoReplyDBHelper extends SQLiteOpenHelper {
     public static final String T4C4 = "cID_Contact";
     private static final String CreateT4 ="CREATE TABLE "+T4Name+" ("+T4Key+" INTEGER PRIMARY KEY, "+
             T4C1+" TEXT , "+
-            T4C2+" VARCHAR, FOREIGN KEY ("+T4C3+") REFERENCES "+T1Name+"("+T1Key+"), "+
-            "FOREIGN KEY ("+T4C4+") REFERENCES "+T2Name+"("+T2Key+"));";
+            T4C2+" VARCHAR,"+T4C3+" INTEGER, "+
+            T4C4+" INTEGER, FOREIGN KEY ("+T4C3+") REFERENCES "+T1Name+"("+T1Key+"), FOREIGN KEY ("+T4C4+") REFERENCES "+T2Name+"("+T2Key+"));";
     public static final String T5Name ="PhoneContact";//Phone Contact Table
     public static final String T5Key ="phoneNumber";
     public static final String T5C1 ="cID_Contact";
     private static final String CreateT5 = "CREATE TABLE "+T5Name+" ("+T5Key+" INTEGER PRIMARY KEY, "+
-            "FOREIGN KEY ("+T5C1+") REFERENCES "+T2Name+"("+T2Key+"));";
+            T5C1+" INTEGER, FOREIGN KEY ("+T5C1+") REFERENCES "+T2Name+"("+T2Key+"));";
     public static final String T6Name = "WiFiConditon";//Wifi Condition
     public static final String T6Key = "wiFiCondID";
     public static final String T6C1 = "wifiName";
@@ -67,8 +67,8 @@ public class AutoReplyDBHelper extends SQLiteOpenHelper {
     public static final String T8C1 ="wiFiID_WifiCond";
     public static final String T8C2 ="GPSID_gpsCond";
     private static final String CreateT8 ="CREATE TABLE "+T8Name+" ("+T8Key+" INTEGER PRIMARY KEY, "+
-            "FOREIGN KEY ("+T8C1+") REFERENCES "+T6Name+" ("+T6Key+"), "+
-            "FOREIGN KEY ("+T8C2+") REFERENCES "+T7Name+" ("+T7Key+"));";
+            T8C1+" INTEGER, "+
+            T8C2+" INTEGER, FOREIGN KEY ("+T8C1+") REFERENCES "+T6Name+" ("+T6Key+"), FOREIGN KEY ("+T8C2+") REFERENCES "+T7Name+" ("+T7Key+"));";
     public static final String T9Name = "ProgMessage";
     public static final String T9Key = "pMessageID";
     public static final String T9C1 = "messText";
@@ -81,15 +81,15 @@ public class AutoReplyDBHelper extends SQLiteOpenHelper {
     public static final String T9C8 = "uID_User";
     private static final String CreateT9="CREATE TABLE "+T9Name +" ("+T9Key+" INTEGER PRIMARY KEY, "+
             T9C1+" VARCHAR , "+T9C2+" TEXT, "+T9C3+" TEXT, "+T9C4+" TEXT, "+T9C5+" INTEGER, "+
-            T9C6+" INTEGER, "+" FOREIGN KEY ("+T9C7+") REFERENCES "+T8Name+" ("+T8Key+")"+
-            "FOREIGN KEY ("+T9C8+") REFERENCE "+T1Name+" ("+T1Key+"));";
+            T9C6+" INTEGER, "+T9C7+" INTEGER, "+
+            T9C8+" INTEGER,  FOREIGN KEY ("+T9C7+") REFERENCES "+T8Name+" ("+T8Key+") FOREIGN KEY ("+T9C8+") REFERENCES "+T1Name+" ("+T1Key+"));";
 
     //DOES NOT CURRENTLY INCLUDE FACEBOOK OR TWITTER CONTACTS, should be new tables given how it's set up
     public static final String T10Name ="PhoneContactList";//Message Contact List Table
     public static final String T10C1 = "pMessageID_ProgMessage";
     public static final String T10C2 = "phoneNumber_PhoneContact";
-    private static final String CreateT10 ="CREATE TABLE "+T10Name+" ( FOREIGN KEY ("+T10C1+") REFERENCES "+T9Name+" ("+T9Key+") , "+
-            "FOREIGN KEY ("+T10C2+") REFERENCES "+T5Name+" ("+T5Key+"), "+
+    private static final String CreateT10 ="CREATE TABLE "+T10Name+" ( "+T10C1+" INTEGER, "+
+            T10C2+" INTEGER, FOREIGN KEY ("+T10C1+") REFERENCES "+T9Name+" ("+T9Key+") ,FOREIGN KEY ("+T10C2+") REFERENCES "+T5Name+" ("+T5Key+"), "+
             "PRIMARY KEY ("+T10C1+", "+T10C2+"));";
 
 
--- app/src/main/java/autotext/app/MainActivity.java
+++ app/src/main/java/autotext/app/MainActivity.java
@@ -17,12 +17,15 @@ public class MainActivity extends AppCompatActivity implements AutoReply.OnFragm
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private final String TAG = "MAIN";
+    protected DatabaseManager data;
+    protected long uID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Activity Created");
         setContentView(R.layout.activity_main);
+        data = new DatabaseManager(this.getApplicationContext());
         goToLogin();
     }
 
@@ -61,7 +64,16 @@ public class MainActivity extends AppCompatActivity implements AutoReply.OnFragm
         goToLogin();
         //disable menu bar buttons here.
     }
-    public void onLogin(){
+    public long checkLogin(String user, String pass){
+        return data.checkUser(user, pass);
+    }
+    public long newUser(String user, String pass){
+
+            return data.addUser(user, pass);
+
+    }
+    public void onLogin(long userID){
+        uID = userID;
         Button go = (Button) findViewById(R.id.auto_reply_button);
         go.setOnClickListener(new View.OnClickListener() {
             @Override
